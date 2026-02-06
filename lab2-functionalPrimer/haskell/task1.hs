4703868f6084418dbd489911981ae6b1611ea077
Mihsan Rashid <kamihame7@gmail.com>
2026-02-06 11:00:59 +0000

    I had written all first 3 tasks out of 7 in the task1 file and not in their respective files. This error has now been rectified.

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -1,18 +1,10 @@
 module Main where
 
 main = do
-    onePlusone
     print (myFunc1 5)
     print (myFunc2 10)
     print (thirdFunc)
-    print (grade 75)
 
 myFunc1 x = x*10
 myFunc2 x = x*2
 thirdFunc = "Hello, this is third Func"
-onePlusone = print "1 + 1 = 2"
-grade score
-   | score >= 90 = "A"
-   | score >= 80 = "B"
-   | score >= 70 = "C"
-   | otherwise = "F"
--- lab2-functionalPrimer/haskell/task3.hs
+++ lab2-functionalPrimer/haskell/task3.hs
@@ -7,9 +7,16 @@ sgn x = if x < 0
 sgnGuards x | x < 0 = -1
             | x == 0 = 0
             | otherwise = 1
+            
+grade score
+   | score >= 90 = "A"
+   | score >= 80 = "B"
+   | score >= 70 = "C"
+   | otherwise = "F"
 
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
     putStrLn( show (sgn (x)) )
+    print (grade 75)
