b5f71e1aeeb09e251ad9c600e5ac2f42c07e44bc
Mihsan Rashid <kamihame7@gmail.com>
2026-02-06 15:01:52 +0000

    Completed Task 5.1 (Using functions as arguments).

--- lab2-functionalPrimer/haskell/func_as_arg.hs
+++ lab2-functionalPrimer/haskell/func_as_arg.hs
@@ -2,12 +2,10 @@
 inpFunc a b = [a..b]
 
 --Define applicatorFunc
-applicatorFunc inpFunc a b s = if s=='s' then (sum inpFunc (a b)) else (sum inpFunc (a b))/((b - a) + 1) 
+applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else sum (inpFunc a b)/b - a + 1 
 
 main = do
-    let result = applicatorFunc inpFunc (2 25) 'a' --Call applicatorFunc with inpFunc and 'a' as args
+    let result = applicatorFunc inpFunc 2 25 'a' --Call applicatorFunc with inpFunc and 'a' as args
     putStrLn("sum = " ++ show(result))
 
--- Brackets added to organise different operations i..e, adding the sum of (value)
--- Those same brackets are universal in terms of application, inpFunc parameters are bracketed
--- Main issues? inpFunc in main doesn't register as valid and "a" at the end of applicationFunc is invalid
+-- Important note: "sum (inpFunc a b)" -> function (function (arguments))
