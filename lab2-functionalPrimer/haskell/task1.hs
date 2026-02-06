7dc7a9b4774219d50e9de1c498c337d90162090b
Mihsan Rashid <kamihame7@gmail.com>
2026-02-06 10:57:42 +0000

    Completed Task 3 Practice Question (Using guards.)

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -5,8 +5,14 @@ main = do
     print (myFunc1 5)
     print (myFunc2 10)
     print (thirdFunc)
+    print (grade 75)
 
 myFunc1 x = x*10
 myFunc2 x = x*2
 thirdFunc = "Hello, this is third Func"
 onePlusone = print "1 + 1 = 2"
+grade score
+   | score >= 90 = "A"
+   | score >= 80 = "B"
+   | score >= 70 = "C"
+   | otherwise = "F"
