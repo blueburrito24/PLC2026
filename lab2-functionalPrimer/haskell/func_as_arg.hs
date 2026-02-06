fc32b70dcfaa4bf7695d057ce259494e01b8524f
Mihsan Rashid <kamihame7@gmail.com>
2026-02-06 15:32:22 +0000

    Completed Task 5.2 (Using Guards alongside Functions as Arguments).

--- lab2-functionalPrimer/haskell/func_as_arg.hs
+++ lab2-functionalPrimer/haskell/func_as_arg.hs
@@ -2,7 +2,11 @@
 inpFunc a b = [a..b]
 
 --Define applicatorFunc
-applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else sum (inpFunc a b)/b - a + 1 
+-- applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else sum (inpFunc a b)/b - a + 1 
+-- below is the same function but written with guards
+applicatorFunc inpFunc a b s
+    | s=='s' = sum (inpFunc a b)
+    | otherwise = sum (inpFunc a b)/b - a + 1
 
 main = do
     let result = applicatorFunc inpFunc 2 25 'a' --Call applicatorFunc with inpFunc and 'a' as args
